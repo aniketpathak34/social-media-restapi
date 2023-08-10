acad48170bb8433c9b073280f88384b2eec8a67b
Aniket Pathak <67203037+aniketpathak34@users.noreply.github.com>
2023-08-10 15:06:02 +0530

    Update settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5&5eyal+!@t!gopvalnc8x0xn^y9if=y1cdmoyadm0lxgv*ee^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
